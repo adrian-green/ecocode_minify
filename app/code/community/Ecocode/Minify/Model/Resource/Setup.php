62105ed037032eb21d62f59754df75468975408d
adrian-green <git@adriangreenwebdesign.com>
2015-12-21 19:37:25 +1000

    setup fix

---
+++ app/code/community/Ecocode/Minify/Model/Resource/Setup.php
@@ -0,0 +1,9 @@
+<?php
+
+class Ecocode_Minify_Model_Resource_Setup extends Mage_Catalog_Model_Resource_Setup
+{
+    public function __construct($resourceName) {
+        parent::__construct($resourceName);
+    }
+
+}
